93354ed971ad3c653855e39d0505d5efc6c09271
thaivm-0609 <vuongthai95@gmail.com>
2024-06-29 16:09:53 +0700

    feat: foreach list

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ app.set('view engine', 'ejs'); //khai báo view engine là ejs
 app.set('views', './views'); //khai báo thư mục chứ file giao diện
 
 //router
+app.get('/list', (req,res) => {
+    let products = [
+        {
+            id: 1,
+            name: 'thaivm2',
+            price: 1000000,
+            description: 'Đây là description',
+            image: 'https://picsum.photos/200'
+        },
+        {
+            id: 3,
+            name: 'thaivm23',
+            price: 3000000,
+            description: 'Đây là description',
+            image: 'https://picsum.photos/200'
+        }
+    ];
+    res.render('list', { products })
+})
+
 app.get('/danhmuc/:iddanhmuc/sanpham/:id', (req,res) => {
     console.log(req.query); //được đánh dấu bằng ?ten1=x&ten2=y trên url
     console.log(req.params); //nằm trong url /:id
